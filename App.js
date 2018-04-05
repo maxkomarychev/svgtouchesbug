a853a1ab1ed19b49e4629c4cecc8e158989954c5
Maksym Komarychev <maxkomarychev@gmail.com>
2018-04-05 11:14:26 +0300

    Sample code

--- App.js
+++ App.js
@@ -4,55 +4,120 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { Alert, Platform, StyleSheet, Text, View } from "react-native";
+import SVG, { G, Circle } from "react-native-svg";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const showEvent = message => {
+  console.log(message);
+  // Alert.alert("Touch handled!", message, [{ text: "OK" }], {
+  //   cancelable: false
+  // });
+};
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{ flex: 1, padding: 20 }}>
+        <Text>Circle</Text>
+        <SVG width="100" height="100">
+          <G>
+            <Circle
+              onPressIn={() => {
+                showEvent("circle onPressIn");
+              }}
+              onPress={() => {
+                showEvent("circle onPress");
+              }}
+              onPressOut={() => {
+                showEvent("circle onPressOut");
+              }}
+              cx="50"
+              cy="50"
+              r="40"
+              stroke="green"
+              strokeWidth="4"
+              fill="yellow"
+            />
+          </G>
+        </SVG>
+
+        <Text>Group</Text>
+        <SVG width="100" height="100">
+          <G
+            onPressIn={() => {
+              showEvent("group onPressIn");
+            }}
+            onPress={() => {
+              showEvent("group onPress");
+            }}
+            onPressOut={() => {
+              showEvent("group onPressOut");
+            }}
+          >
+            <Circle
+              cx="50"
+              cy="50"
+              r="40"
+              stroke="green"
+              strokeWidth="4"
+              fill="yellow"
+            />
+          </G>
+        </SVG>
+
+        <Text>Circle onPress</Text>
+        <SVG width="100" height="100">
+          <G>
+            <Circle
+              onPress={() => {
+                showEvent("circle onPress");
+              }}
+              cx="50"
+              cy="50"
+              r="40"
+              stroke="green"
+              strokeWidth="4"
+              fill="yellow"
+            />
+          </G>
+        </SVG>
+        <Text>Circle onPressIn + onPress</Text>
+        <SVG width="100" height="100">
+          <G>
+            <Circle
+              onPressIn={() => {
+                showEvent("circle onPressIn");
+              }}
+              onPress={() => {
+                showEvent("circle onPress");
+              }}
+              cx="50"
+              cy="50"
+              r="40"
+              stroke="green"
+              strokeWidth="4"
+              fill="yellow"
+            />
+          </G>
+        </SVG>
+        <Text>Circle onPressOut</Text>
+        <SVG width="100" height="100">
+          <G>
+            <Circle
+              onPressOut={() => {
+                showEvent("circle onPressOut");
+              }}
+              cx="50"
+              cy="50"
+              r="40"
+              stroke="green"
+              strokeWidth="4"
+              fill="yellow"
+            />
+          </G>
+        </SVG>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
